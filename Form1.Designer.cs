d72f54e3b52042b9b538d55d3584aefffbe2e343
Derik <Derik.p.schaefer@gmail.com>
2023-01-17 14:43:54 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace MMGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.resultsLabel = new System.Windows.Forms.Label();
+            this.guessButton = new System.Windows.Forms.Button();
+            this.guessTextBox = new System.Windows.Forms.RichTextBox();
+            this.guessesRemainingLabel = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pastGuessesListBox = new System.Windows.Forms.RichTextBox();
+            this.MasterMindLabel = new System.Windows.Forms.Label();
+            this.OneMoreBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // resultsLabel
+            // 
+            this.resultsLabel.AutoSize = true;
+            this.resultsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.resultsLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(231)))), ((int)(((byte)(177)))));
+            this.resultsLabel.Location = new System.Drawing.Point(216, 282);
+            this.resultsLabel.Name = "resultsLabel";
+            this.resultsLabel.Size = new System.Drawing.Size(0, 13);
+            this.resultsLabel.TabIndex = 0;
+            // 
+            // guessButton
+            // 
+            this.guessButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(135)))), ((int)(((byte)(110)))));
+            this.guessButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(231)))), ((int)(((byte)(177)))));
+            this.guessButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.guessButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.guessButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(231)))), ((int)(((byte)(177)))));
+            this.guessButton.Location = new System.Drawing.Point(138, 393);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(75, 23);
+            this.guessButton.TabIndex = 2;
+            this.guessButton.Text = "Guess";
+            this.guessButton.UseVisualStyleBackColor = false;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click_1);
+            // 
+            // guessTextBox
+            // 
+            this.guessTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(187)))), ((int)(((byte)(141)))));
+            this.guessTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(231)))), ((int)(((byte)(177)))));
+            this.guessTextBox.Location = new System.Drawing.Point(219, 356);
+            this.guessTextBox.Name = "guessTextBox";
+            this.guessTextBox.Size = new System.Drawing.Size(310, 60);
+            this.guessTextBox.TabIndex = 3;
+            this.guessTextBox.Text = "";
+            // 
+            // guessesRemainingLabel
+            // 
+            this.guessesRemainingLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(187)))), ((int)(((byte)(141)))));
+            this.guessesRemainingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessesRemainingLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(98)))), ((int)(((byte)(85)))));
+            this.guessesRemainingLabel.Location = new System.Drawing.Point(118, 48);
+            this.guessesRemainingLabel.Name = "guessesRemainingLabel";
+            this.guessesRemainingLabel.Size = new System.Drawing.Size(115, 96);
+            this.guessesRemainingLabel.TabIndex = 4;
+            this.guessesRemainingLabel.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(231)))), ((int)(((byte)(177)))));
+            this.label1.Location = new System.Drawing.Point(115, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Remaining Guesses";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(231)))), ((int)(((byte)(177)))));
+            this.label2.Location = new System.Drawing.Point(216, 340);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Input your guess here";
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(135)))), ((int)(((byte)(110)))));
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.startButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(231)))), ((int)(((byte)(177)))));
+            this.startButton.Location = new System.Drawing.Point(702, 356);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 8;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click_1);
+            // 
+            // resetButton
+            // 
+            this.resetButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(135)))), ((int)(((byte)(110)))));
+            this.resetButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(231)))), ((int)(((byte)(177)))));
+            this.resetButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.resetButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(231)))), ((int)(((byte)(177)))));
+            this.resetButton.Location = new System.Drawing.Point(702, 393);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 9;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(231)))), ((int)(((byte)(177)))));
+            this.label3.Location = new System.Drawing.Point(12, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Past Guesses";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // pastGuessesListBox
+            // 
+            this.pastGuessesListBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(187)))), ((int)(((byte)(141)))));
+            this.pastGuessesListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pastGuessesListBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(98)))), ((int)(((byte)(85)))));
+            this.pastGuessesListBox.Location = new System.Drawing.Point(12, 48);
+            this.pastGuessesListBox.Name = "pastGuessesListBox";
+            this.pastGuessesListBox.Size = new System.Drawing.Size(100, 247);
+            this.pastGuessesListBox.TabIndex = 11;
+            this.pastGuessesListBox.Text = "";
+            // 
+            // MasterMindLabel
+            // 
+            this.MasterMindLabel.AutoSize = true;
+            this.MasterMindLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MasterMindLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(231)))), ((int)(((byte)(177)))));
+            this.MasterMindLabel.Location = new System.Drawing.Point(259, 9);
+            this.MasterMindLabel.Name = "MasterMindLabel";
+            this.MasterMindLabel.Size = new System.Drawing.Size(328, 39);
+            this.MasterMindLabel.TabIndex = 12;
+            this.MasterMindLabel.Text = "Master Mind Game";
+            // 
+            // OneMoreBTN
+            // 
+            this.OneMoreBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(135)))), ((int)(((byte)(110)))));
+            this.OneMoreBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OneMoreBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.OneMoreBTN.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(231)))), ((int)(((byte)(177)))));
+            this.OneMoreBTN.Location = new System.Drawing.Point(118, 150);
+            this.OneMoreBTN.Name = "OneMoreBTN";
+            this.OneMoreBTN.Size = new System.Drawing.Size(115, 23);
+            this.OneMoreBTN.TabIndex = 14;
+            this.OneMoreBTN.Text = "One More Turn";
+            this.OneMoreBTN.UseVisualStyleBackColor = false;
+            this.OneMoreBTN.Click += new System.EventHandler(this.OneMoreBTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(98)))), ((int)(((byte)(85)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OneMoreBTN);
+            this.Controls.Add(this.MasterMindLabel);
+            this.Controls.Add(this.pastGuessesListBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guessesRemainingLabel);
+            this.Controls.Add(this.guessTextBox);
+            this.Controls.Add(this.guessButton);
+            this.Controls.Add(this.resultsLabel);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label resultsLabel;
+        private System.Windows.Forms.Button guessButton;
+        private System.Windows.Forms.RichTextBox guessTextBox;
+        private System.Windows.Forms.RichTextBox guessesRemainingLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox pastGuessesListBox;
+        private System.Windows.Forms.Label MasterMindLabel;
+        private System.Windows.Forms.Button OneMoreBTN;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MMGame
+{
+    public partial class Form1 : Form
+    {
+        private List<char> solution;
+        private
+        const int MAX_GUESSES = 10;
+        private HttpClient client = new HttpClient();
+        private int guesses;
+
+        public Form1()
+        {
+
+            // exception handling
+            try
+            {
+                InitializeComponent();
+                // Hiding buttons that should not be pressed at this point in the game
+                guessButton.Enabled = false;
+                resetButton.Enabled = false;
+                guessButton.Visible = false;
+                resetButton.Visible = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while starting the game: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            // calling my CreateSolution async method, trying to improve performance w/ the API
+            CreateSolution();
+            guessesRemainingLabel.Text = MAX_GUESSES.ToString();
+        }
+        private async Task CreateSolution()
+        {
+            // calling API, checking if succesful
+            var numbers = await client.GetAsync("https://www.random.org/integers/?num=4&min=1&max=6&col=1&base=10&format=plain&rnd=new");
+            if (numbers.IsSuccessStatusCode)
+            {
+                // API appears to return number + white space / line break. Trying to mitigate that here
+                var message = await numbers.Content.ReadAsStringAsync();
+                solution = new List<char>();
+                string[] numsString = message.TrimEnd('\n').Split('\n');
+
+                foreach (string num in numsString)
+                {
+                    solution.Add(char.Parse(num));
+                }
+            }
+        }
+
+        private async void startButton_Click_1(object sender, EventArgs e)
+        {
+            // Async, await on the API creating the solution
+            await CreateSolution();
+            MessageBox.Show("The game is starting! Please enter a 4 digit number as your guess. The format should be xxxx. You have " + MAX_GUESSES + " guesses to find the correct solution. Good luck!", "Game Started", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            guessButton.Enabled = true;
+            resetButton.Enabled = true;
+            guessButton.Visible = true;
+            resetButton.Visible = true;
+            // resetting guess amount and displaying that in the guesses remaining box
+            guesses = 0;
+            guessesRemainingLabel.Text = MAX_GUESSES.ToString();
+            resultsLabel.Text = "";
+        }
+        private void guessButton_Click_1(object sender, EventArgs e)
+        {
+            // try / catch / finally exception handling 
+            try
+            {
+                // First assess whether or not the player has guessed more times than allowed, if true then end the game
+                if (guesses >= MAX_GUESSES)
+                {
+                    resultsLabel.Text = "Sorry, you did not guess the solution within " + MAX_GUESSES + " guesses. The solution was " + string.Join("", solution.ToArray());
+                    guessButton.Enabled = false;
+                    OneMoreBTN.Enabled = false;
+                    return;
+                }
+                // parse the user's guess input, check for valid input (4 numbers), display message if incorrect input
+                string guess = guessTextBox.Text;
+                if (guess.Length != 4)
+                {
+                    resultsLabel.Text = "Invalid input. Please enter a 4 digit number.";
+                    return;
+                }
+                // Adding user's guess to past guess box
+                pastGuessesListBox.AppendText(guess + Environment.NewLine);
+                guesses++;
+                guessesRemainingLabel.Text = (MAX_GUESSES - guesses).ToString();
+
+                // creating variables for correct digits and correct positions here and then assessing for truthyness
+                int correctDigits = 0;
+                int correctPositions = 0;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    if (guess[i] == solution[i])
+                    {
+                        correctPositions++;
+                    }
+                    else if (solution.Contains(guess[i]))
+                    {
+                        correctDigits++;
+                    }
+                }
+
+                // Return results to user in results label
+                resultsLabel.Text = correctPositions + " digits in the correct position, " + correctDigits + " digits correct but in the wrong position.";
+                guessTextBox.Text = "";
+            }
+            catch (Exception ex)
+            {
+                // Generic catch block to handle exceptions
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void resetButton_Click_1(object sender, EventArgs e)
+        {
+            // Calling update UI method, recreating a new solution for a new game
+            UpdateUI();
+            await CreateSolution();
+            pastGuessesListBox.Clear();
+        }
+
+        private void UpdateUI()
+        {
+            // Guess button not enabled until game is started, resetting guess amount and displaying that, clearing out text boxes. This will
+            // give the apperane of starting a new game
+            guessButton.Enabled = false;
+            guesses = 0;
+            guessesRemainingLabel.Text = MAX_GUESSES.ToString();
+            guessTextBox.Clear();
+            resultsLabel.Text = "";
+        }
+
+        private void OneMoreBTN_Click(object sender, EventArgs e)
+        {
+            // Additional feature I thought of, this gives the user an additional guess when they click this button. Only works if clicked
+            // prior to exceeding max guesses
+            if (guesses < MAX_GUESSES)
+            {
+                // I convert the information within the guessesremaining box to an int, add one to it and then convert back to a string
+                guessesRemainingLabel.Text = (int.Parse(guessesRemainingLabel.Text) + 1).ToString();
+            }
+        }
+    }
+}
